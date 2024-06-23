2e86ece5f63ce36bbc55235378848ddc44b91d72
Pablinskv <Pablo.esquivel2009@gmail.com>
2024-06-23 14:06:01 -0300

    con excepciones

--- Autoparte.php
+++ Autoparte.php
@@ -5,6 +5,14 @@ class Autoparte {
     private $desgaste;
 
     public function __construct($nombre) {
+        if (empty($nombre)) {
+            throw new Exception("El nombre de la autopieza no puede estar vacío");
+        }
+    
+        if (!preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ_ ]+$/', $nombre)) {
+            throw new Exception("El nombre de la autopieza solo puede contener caracteres alfanuméricos, guiones bajos y espacios");
+        }
+    
         $this->nombre = $nombre;
         $this->desgaste = 0;
     }
@@ -15,6 +23,10 @@ class Autoparte {
     }
     // Aumenta el desgaste en 1.
     public function usar() {
+        if ($this->desgaste < 0) {
+            throw new Exception("El desgaste de la autopieza no puede ser negativo");
+        }
+    
         $this->desgaste++;
     }
 }
