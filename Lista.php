dfe5539f2a110bf5613f01a4d35fca973974c182
Joelkukin <joelkukin@gmail.com>
2024-06-23 12:16:55 -0300

    Clase "Lista" terminada y testeada

--- Lista.php
+++ Lista.php
@@ -1,30 +1,79 @@
 <?php
-    class Lista(){
-        public $lista = array();
-        function __construct(...$elementos){
-            $this->lista = $elementos;
+    class Lista{
+        protected $contenido = array();
+        function __construct(Object ...$elementos){
+            $this->contenido = $elementos;
+        }
+        function ver(){
+            return $this->contenido;
         }
 
         function agregar(Object $elemento){
-            array_push($this->lista, $elemento);
+            array_push($this->contenido, $elemento);
+            return $this->ver();
         }
 
 
-        function buscar (String $propiedad, $valor_buscado){
-            foreach ($this->lista as $index => $elemento) {
-                if ($elemento[$propiedad] === $valor_buscado) {
-                    return $elemento;
+        public function buscar($propiedad, $valor, $posicion = 0) {
+            $resultados = []; // Array para almacenar los objetos encontrados
+    
+            foreach ($this->contenido as $indice=>$objeto) {
+                // Verifica si la propiedad existe en el objeto
+                if (property_exists($objeto, $propiedad)) {
+                    // Compara el valor de la propiedad con el valor buscado
+                    if ($objeto->$propiedad === $valor) {
+                        $resultados[] = [$indice, $objeto]; // Agrega el objeto al array de resultados
+                    }
                 }
+            }
+            if($posicion-1 >= 0){
+                return $resultados[$posicion-1];
+            }else{
+                if(count($resultados) == 1){
+                    return $resultados[0]; // Devuelve el array con los objetos encontrados
+                }
+                return $resultados; // Devuelve el array con los objetos encontrados
+
             }
         }
 
-        function reemplazar (Object $objeto_viejo, Object $objeto_nuevo){
-            array_replace($objeto_viejo, $objeto_nuevo)
+        function reemplazar ($objeto_viejo, $objeto_nuevo){
+            if(!is_array($objeto_viejo)){
+                $posicion = array_search($objeto_viejo, $this->contenido);
+            } else {
+                [$posicion, $objeto_viejo] = $objeto_viejo;
+            }
+
+            if ($posicion !== false) {
+                $this->contenido[$posicion] = $objeto_nuevo;
+            }
+                
+            return $this->ver();
         }
-        function eliminar (Object $objeto_a_eliminar){
-            array_diff($this->lista, array($objeto_a_eliminar));
+
+
+        function eliminar ($objeto_a_eliminar){
+            switch (gettype($objeto_a_eliminar)) {
+                case 'array':
+                    [$posicion, $objeto_a_eliminar] = $objeto_a_eliminar;
+                    break;
+                
+                case 'integer':
+                    $posicion = $objeto_a_eliminar;
+                    break;
+                
+                case 'object':
+                    $posicion = array_search($objeto_a_eliminar, $this->contenido);
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+
+            unset($this->contenido[$posicion]);
+            return $this->ver();
         }
     }
     
-    $lista = new Lista([])
 ?>
---
+++ test_Lista.php
@@ -0,0 +1,43 @@
+<?php
+    include_once "./Lista.php";
+    include_once "./Autoparte.php";
+    include_once "./utils.php";
+
+     $autopartes = new Lista(
+        new Autoparte("motor"), 
+        new Autoparte("transmision"),
+        new Autoparte("rueda"),
+        new Autoparte("rueda"),
+        new Autoparte("rueda"),
+        new Autoparte("rueda"),
+        new Autoparte("chasis"),
+        new Autoparte("freno"),
+        new Autoparte("freno"),
+        new Autoparte("freno")
+    );
+
+    
+
+    
+    $freno = new Autoparte("freno");
+    $valor = "nombre";
+    $propiedad = "nombre";
+    var_dump($freno->$propiedad)."\n";
+    
+    print_r("<h3>Metodo ver</h3>");
+    echo json_encode($autopartes->ver(), JSON_PRETTY_PRINT)."<br>";
+    
+    print_r("<h3>Metodo agregar</h3>");
+    echo json_encode($autopartes->agregar(new Autoparte("freno")), JSON_PRETTY_PRINT)."<br>";
+    
+    print_r("<h3>Metodo buscar</h3>");
+    echo json_encode($autopartes->buscar("nombre", "freno"), JSON_PRETTY_PRINT)."<br>";
+
+    print_r("<h3>Metodo reemplazar</h3>");
+    echo json_encode($autopartes->reemplazar($autopartes->buscar("nombre", "freno",2), new Autoparte("frBrandstorm")), JSON_PRETTY_PRINT)."<br><br>";
+    
+    print_r("<h3>Metodo eliminar</h3>");
+    echo json_encode($autopartes->eliminar($autopartes->buscar("nombre", "frBrandstorm")), JSON_PRETTY_PRINT)."<br>"; 
+    echo "<br>".json_encode($autopartes->eliminar(9), JSON_PRETTY_PRINT)."<br>"; 
+    echo "<br>".json_encode($autopartes->eliminar(new Autoparte("motor")), JSON_PRETTY_PRINT)."<br>"; 
+?>
